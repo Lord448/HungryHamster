8c9ec5e27560aed70f84a536b618c6f4308aa121
Lord448 <pedroeroca@outlook.com>
2023-11-04 22:54:35 -0600

    Window appear in second monitor

--- desktop/src/ca/crit/hungryhamster/DesktopLauncher.java
+++ desktop/src/ca/crit/hungryhamster/DesktopLauncher.java
@@ -1,16 +1,24 @@
 package ca.crit.hungryhamster;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-import java.util.Arrays;
+import javax.management.monitor.Monitor;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		GameHandler.init(0, GameHandler.DESKTOP_ENV);
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		//Only valid when 2 monitors connected
+		Graphics.Monitor[] monitors = Lwjgl3ApplicationConfiguration.getMonitors();
+		Graphics.DisplayMode displayMode = Lwjgl3ApplicationConfiguration.getDisplayMode(monitors[1]);
+		int posX = monitors[1].virtualX + displayMode.width/2 - 640/2;
+		int posY = monitors[1].virtualY + displayMode.height/2 - 480/2;
 		config.setWindowedMode(480, 640);
+		config.setWindowPosition(posX, posY);
+
 		config.setForegroundFPS(60);
 		config.setTitle("HungryHamster");
 		new Lwjgl3Application(new Main_hungryHamster(), config);
