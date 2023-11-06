b158b343b43262101f462513d48a6406eaf4bab7
Lord448 <pedroeroca@outlook.com>
2023-11-05 23:33:23 -0600

    Reorganazing the GameHandler

--- core/src/ca/crit/hungryhamster/GameHandler.java
+++ core/src/ca/crit/hungryhamster/GameHandler.java
@@ -39,18 +39,28 @@ import static com.badlogic.gdx.Input.Keys.Z;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import ca.crit.hungryhamster.time.Time;
+import ca.crit.hungryhamster.time.TimeMillis;
 
 public class GameHandler {
-    //Global definitions for debug
+    /**
+     * --------------------------------------------------------------------------
+     *                        GLOBAL DEFINITIONS FOR DEBUG
+     * --------------------------------------------------------------------------
+    */
     public static final int DEBUG_GAME = 0;
     public static final int DEBUG_MENU = 1;
     public static final int DEBUG_DB = 2;
     public static final int DEBUG_NONE = 3;
     public static final int DEBUG_MODE = DEBUG_GAME; //Debug constant
-    //Global Constants
+    /**
+     * --------------------------------------------------------------------------
+     *                              GLOBAL CONSTANTS
+     * --------------------------------------------------------------------------
+     */
     public static final int LADDER_MAX_STEPS = 32;
     public static final int DESKTOP_ENV = 0;
     public static final int MOBILE_ENV = 1;
@@ -62,30 +72,59 @@ public class GameHandler {
     //Only applies on stages
     public static int NATIVE_RES_WIDTH = 480;
     public static int NATIVE_RES_HEIGHT = 640;
-    //Global variables
-    public static int environment;
+    /**
+     * --------------------------------------------------------------------------
+     *                       CONFIGURATION GLOBAL VARIABLES
+     * --------------------------------------------------------------------------
+     */
+    public static float musicVolume;
+    public static float effectsVolume;
     public static int numHouseSteps = 11;
     public static int maxStep;
     public static int minStep;
     public static int extraStep = 0;
-    public static Time sessionTime = new Time();
-    public static List<Time> meanTimeOnStep = new ArrayList<Time>();
-    public static List<Time> repsTime = new ArrayList<Time>();
-    public static int sessionReps = 0;
-    public static int sessionUncompletedReps;
-    public static final double animHysteresis = 0.30;
-    public static float musicVolume;
-    public static float effectsVolume;
-    public static boolean wizardSpell  = false;
-    public static float[] foodPositions;
     public static int countsToWin = numHouseSteps;
+    /**
+     * --------------------------------------------------------------------------
+     *                              GLOBAL VARIABLES
+     * --------------------------------------------------------------------------
+     */
+    public static int environment;
+    public static final double animHysteresis = 0.30;
+    public static boolean wizardSpell  = false;
     public static boolean startGame = false;
-    public static boolean[] touchPins = new boolean[LADDER_MAX_STEPS];
+    public static float[] foodPositions;
+    /**
+     * --------------------------------------------------------------------------
+     *                        PATIENT PERFORMANCE MEASURES
+     * --------------------------------------------------------------------------
+     */
+    public static Time sessionTime = new Time(); //Time of the session
+    public static TimeMillis meanSessionTimeStep; //Time to complete a step per session
+    public static List<TimeMillis> meanRepTimeStep = new ArrayList<TimeMillis>(); //Time to complete step per Repetition
+    public static List<Time> repsTime = new ArrayList<Time>(); //Time to complete the repetition
+    public static int sessionReps = 0; //Repetitions of the session
+    public static int sessionUncompletedReps; //Uncompleted repetitions on the session
+    public static int successfulSteps; //Successful steps on the session
+
+    /**
+     * --------------------------------------------------------------------------
+     *                            PATIENT INFORMATION
+     * --------------------------------------------------------------------------
+     */
     public static String playerID;
     public static String playerName;
     public static String playerGender;
     public static int playerWorkingHand;
     public static int playerAge;
+    public static int playerLastNumSteps;
+    public static Date currentDate;
+    /**
+     * --------------------------------------------------------------------------
+     *                         GAME INTERFACE & INTERACT
+     * --------------------------------------------------------------------------
+     */
+    public static boolean[] touchPins = new boolean[LADDER_MAX_STEPS];
     public static final int[] key = {
             NUM_0, NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6, NUM_7, NUM_8, NUM_9,
             Q, W, E, R, T, Y, U, I, O, P,
@@ -93,7 +132,11 @@ public class GameHandler {
             Z, X, C, V, B, N, M
     };
     public static final String[] strReceptions = new String[numHouseSteps];
-    
+    /**
+     * --------------------------------------------------------------------------
+     *                                 METHODS
+     * --------------------------------------------------------------------------
+     */
     public static void init(float musicVolume, int env) {
         //Extract the ID's
         //Setting global variables
